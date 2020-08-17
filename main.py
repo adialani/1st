7c556f57f15d6b1ff4d4240ec6feb9a17e5e0403
adialani <dialaashna@gmail.com>
2020-08-17 19:11:43 +0000

    s

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ speed = pygame.math.Vector2(0,3)
 rotation = random.randint(0,360)
 speed.rotate_ip(rotation)
 
+if fish_rect.left < 0 or fish_rect.right > screen_info.current_w:
+	speed[0]*= -1
+	fish_image = pygame.transform.flip(fish_image, True, False)
+	fish_rect.move_ip(speed[0, 0])
+
+if fish_rect.top < 0 or fish_rect.bottom >screen_info.current_h:
+	speed[1] *= -1
+	fish_image = pygame.transform.flip(fish_image, False, True)
+	fish_rect.move_ip(speed[1],0)
+
 def move_fish():
 	fish_rect.move_ip(speed)
 
